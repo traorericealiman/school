747687eb692c8ab34eb69a375a28ff303ecb4025
Bachi1698 <maryliseallegou@gmail.com>
2020-04-12 03:12:18 +0100

    intergration de template dans django

---
+++ learnplus/Learn/instructor/urls.py
@@ -0,0 +1,29 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index_2, name='index-2'),
+    path('fixed_instructor_account_edit', views.fixed_instructor_account_edit, name='fixed-instructor-account-edit'),
+    path('fixed_instructor_browse_courses', views.fixed_instructor_browse_courses, name='fixed-instructor-browse-courses'),
+    path('fixed_instructor_carts', views.fixed_instructor_carts, name='fixed-instructor-cart'),
+    path('fixed_instructor_course_edit', views.fixed_instructor_course_edit, name='fixed-instructor-course-edit'),
+    path('fixed_instructor_courses', views.fixed_instructor_courses, name='fixed-instructor-courses'),
+    path('fixed_instructor_dashboard', views.fixed_instructor_dashboard, name='fixed-instructor-dashboard'),
+    path('fixed_instructor_earnings', views.fixed_instructor_earnings, name='fixed-instructor-earnings'),
+    path('fixed_instructor_edit_invoice', views.fixed_instructor_edit_invoice, name='fixed-instructor-edit-invoice'),
+    path('fixed_instructor_forum', views.fixed_instructor_forum, name='fixed-instructor-forum'),
+    path('fixed_instructor_forum_ask', views.fixed_instructor_forum_ask, name='fixed-instructor-forum-ask'),
+    path('fixed_instructor_forum_thread', views.fixed_instructor_forum_thread, name='fixed-instructor-forum-thread'),
+    path('fixed_instructor_invoice', views.fixed_instructor_invoice, name='fixed-instructor-invoice'),
+    path('fixed_instructor_invoice_settings', views.fixed_instructor_invoice_settings, name='fixed-instructor-invoice-settings'),
+    path('fixed_instructor_messages', views.fixed_instructor_messages, name='fixed-instructor-messages'),
+    path('fixed_instructor_messages_2', views.fixed_instructor_messages_2, name='fixed-instructor-messages-2'),
+    path('fixed_instructor_profile', views.fixed_instructor_profile, name='fixed-instructor-profile'),
+    path('fixed_instructor_quiz_edit', views.fixed_instructor_quiz_edit, name='fixed-instructor-quiz-edit'),
+    path('fixed_instructor_quiz_results', views.fixed_instructor_quiz_results, name='fixed-instructor-quiz-results'),
+    path('fixed_instructor_quizzes', views.fixed_instructor_quizzes, name='fixed-instructor-quizzes'),
+    path('fixed_instructor_take_course', views.fixed_instructor_take_course, name='fixed-instructor-take-course'),
+    path('fixed_instructor_take_quiz', views.fixed_instructor_take_quiz, name='fixed-instructor-take-quiz'),
+    path('fixed_instructor_view_course', views.fixed_instructor_view_course, name='fixed-instructor-view-course'),
+    
+]
---
+++ learnplus/Learn/instructor/views.py
@@ -0,0 +1,169 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index_2(request):
+    datas = {
+
+    }
+    return render(request,'pages/index-2.html',datas)
+
+def fixed_instructor_account_edit(request):
+    datas = {
+
+    }
+    return render(request,'pages/fixed-instructor-account-edit.html',datas)
+
+def fixed_instructor_browse_courses(request):
+    datas = {
+
+    }
+    return render(request,'pages/fixed-instructor-browse-courses.html',datas)
+
+def fixed_instructor_carts(request):
+    datas = {
+
+    }
+    return render(request,'pages/fixed-instructor-cart.html',datas)
+
+def fixed_instructor_course_edit(request):
+    datas = {
+
+    }
+    return render(request,'pages/fixed-instructor-course-edit.html',datas)
+
+
+def fixed_instructor_courses(request):
+    datas = {
+
+    }
+    return render(request,'pages/fixed-instructor-courses.html',datas)
+
+def fixed_instructor_dashboard(request):
+    datas = {
+
+    }
+    return render(request,'pages/fixed-instructor-dashboardx.html',datas)
+
+def fixed_instructor_earnings(request):
+    datas = {
+
+    }
+    return render(request,'pages/fixed-instructor-earnings.html',datas)
+
+def fixed_instructor_edit_invoice(request):
+    datas = {
+
+    }
+    return render(request,'pages/fixed-instructor-edit-invoice',datas)
+
+def fixed_instructor_forum(request):
+    datas = {
+
+    }
+    return render(request,'pages/fixed-instructor-forum.html',datas)
+
+def fixed_instructor_forum_ask(request):
+    datas = {
+
+    }
+    return render(request,'pages/fixed-instructor-forum-ask.html',datas)
+
+def fixed_instructor_forum_thread(request):
+    datas = {
+
+    }
+    return render(request,'pages/fixed-instructor-forum-thread.html',datas)
+
+def fixed_instructor_invoice(request):
+    datas = {
+
+    }
+    return render(request,'pages/fixed-instructor-invoice.html',datas)
+
+def fixed_instructor_invoice_settings(request):
+    datas = {
+
+    }
+    return render(request,'pages/fixed-instructor-invoice-settings.html',datas)
+
+def fixed_instructor_lesson_add(request):
+    datas = {
+
+    }
+    return render(request,'pages/fixed-instructor-lesson-add.html',datas)
+
+def fixed_instructor_messages(request):
+    datas = {
+
+    }
+    return render(request,'pages/fixed-instructor-messages.html',datas)
+
+def fixed_instructor_messages_2(request):
+    datas = {
+
+    }
+    return render(request,'pages/fixed-instructor-messages-2.html',datas)
+
+def fixed_instructor_my_courses(request):
+    datas = {
+
+    }
+    return render(request,'pages/fixed-instructor-my-courses.html',datas)
+
+def fixed_instructor_profile(request):
+    datas = {
+
+    }
+    return render(request,'pages/fixed-instructor-profile.html',datas)
+
+def fixed_instructor_quiz_edit(request):
+    datas = {
+
+    }
+    return render(request,'pages/fixed-instructor-quiz-edit.html',datas)
+
+def fixed_instructor_quiz_results(request):
+    datas = {
+
+    }
+    return render(request,'pages/fixed-instructor-quiz-results.html',datas)
+
+def fixed_instructor_quizzes(request):
+    datas = {
+
+    }
+    return render(request,'pages/fixed-instructor-quizzes.html',datas)
+
+def fixed_instructor_review_quiz(request):
+    datas = {
+
+    }
+    return render(request,'pages/fixed-instructor-review-quiz.html',datas)
+
+def fixed_instructor_statement(request):
+    datas = {
+
+    }
+    return render(request,'pages/fixed-instructor-statementr.html',datas)
+
+def fixed_instructor_take_course(request):
+    datas = {
+
+    }
+    return render(request,'pages/fixed-instructor-take-course.html',datas)
+
+def fixed_instructor_take_quiz(request):
+    datas = {
+
+    }
+    return render(request,'pages/fixed-instructor-take-quiz.html',datas)
+
+def fixed_instructor_view_course(request):
+    datas = {
+
+    }
+    return render(request,'pages/fixed-instructor-view-course.html',datas)
+
+
+
+
