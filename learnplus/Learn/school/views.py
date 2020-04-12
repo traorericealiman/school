6a234626c88fb7c79faee5fc69b45967f8e8e64c
Bachi1698 <maryliseallegou@gmail.com>
2020-04-12 23:10:42 +0100

    page school

--- learnplus/Learn/school/views.py
+++ learnplus/Learn/school/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
 # Create your views here.
+def guest_login(request):
+
+    datas = {
+
+    }
+    return render(request, 'pages/guest-login.html', datas)
+
+def guest_signup(request):
+
+    datas = {
+
+    }
+    return render(request, 'pages/guest-signup.html', datas) 
+
+def guest_forgot_password(request):
+
+    datas = {
+
+    }
+    return render(request, 'pages/guest-forgot-password.html', datas)
