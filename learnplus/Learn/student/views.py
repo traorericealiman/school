6b1db5fa3168f2997194398dfc84ef9aed08dd62
Bachi1698 <maryliseallegou@gmail.com>
2020-04-12 22:31:09 +0100

    page student

--- learnplus/Learn/student/views.py
+++ learnplus/Learn/student/views.py
@@ -8,3 +8,190 @@ def index(request):
 
     }
     return render(request, 'pages/fixed-student-dashboard.html', datas)
+
+def fixed_student_account_billing_payment_information(request):
+    datas = {
+
+    }
+    return render(request,'pages/fixed-student-account-billing-payment-information.html',datas)
+
+def fixed_student_account_billing_subscription(request):
+    datas = {
+
+    }
+    return render(request,'pages/fixed-student-account-billing-subscription.html',datas)
+
+def fixed_student_account_billing_upgrade(request):
+    datas = {
+
+    }
+    return render(request,'pages/fixed-student-account-billing-upgrade.html',datas)
+
+def fixed_student_account_edit(request):
+    datas = {
+
+    }
+    return render(request,'pages/fixed-student-account-edit.html',datas)
+
+def fixed_student_account_edit_basic(request):
+    datas = {
+
+    }
+    return render(request,'pages/fixed-student-account-edit-basic.html',datas)
+
+def fixed_student_account_edit_profile(request):
+    datas = {
+
+    }
+    return render(request,'pages/fixed-student-account-edit-profile.html',datas)
+
+def fixed_student_billing(request):
+    datas = {
+
+    }
+    return render(request,'pages/fixed-student-billing.html',datas)
+
+def fixed_student_browse_courses(request):
+    datas = {
+
+    }
+    return render(request,'pages/fixed-student-browse-courses.html',datas)
+
+def fixed_student_cart(request):
+    datas = {
+
+    }
+    return render(request,'pages/fixed-student-cart.html',datas)
+
+def fixed_student_courses(request):
+    datas = {
+
+    }
+    return render(request,'pages/fixed-student-courses.html',datas)
+
+def fixed_student_dashboard(request):
+    datas = {
+
+    }
+    return render(request,'pages/fixed-student-dashboard.html',datas)
+
+def fixed_student_earnings(request):
+    datas = {
+
+    }
+    return render(request,'pages/fixed-student-earnings.html',datas)
+
+
+def fixed_student_forum(request):
+    datas = {
+
+    }
+    return render(request,'pages/fixed-student-forum.html',datas)
+
+def fixed_student_forum_ask(request):
+    datas = {
+
+    }
+    return render(request,'pages/fixed-student-forum-ask.html',datas)
+
+def fixed_student_forum_thread(request):
+    datas = {
+
+    }
+    return render(request,'pages/fixed-student-forum-thread.html',datas)
+
+def fixed_student_help_center(request):
+    datas = {
+
+    }
+    return render(request,'pages/fixed-student-help-center.html',datas)
+
+def fixed_student_invoice(request):
+    datas = {
+
+    }
+    return render(request,'pages/fixed-student-invoice.html',datas)
+
+def fixed_student_messages(request):
+    datas = {
+
+    }
+    return render(request,'pages/fixed-student-messages.html',datas)
+
+def fixed_student_messages_2(request):
+    datas = {
+
+    }
+    return render(request,'pages/fixed-student-messages-2.html',datas)
+
+def fixed_student_my_courses(request):
+    datas = {
+
+    }
+    return render(request,'pages/fixed-student-my-courses.html',datas)
+
+def fixed_student_pay(request):
+    datas = {
+
+    }
+    return render(request,'pages/fixed-student-pay.html',datas)
+
+def fixed_student_profile(request):
+    datas = {
+
+    }
+    return render(request,'pages/fixed-student-profile.html',datas)
+
+def fixed_student_profile_posts(request):
+    datas = {
+
+    }
+    return render(request,'pages/fixed-student-profile-posts.html',datas)
+
+def fixed_student_quiz_results(request):
+    datas = {
+
+    }
+    return render(request,'pages/fixed-student-quiz-results.html',datas)
+
+def fixed_student_quizzes(request):
+    datas = {
+
+    }
+    return render(request,'pages/fixed-student-quizzes.html',datas)
+
+def fixed_student_signup(request):
+    datas = {
+
+    }
+    return render(request,'pages/fixed-student-signup.html',datas)
+
+def fixed_student_statement(request):
+    datas = {
+
+    }
+    return render(request,'pages/fixed-student-statement.html',datas)
+
+def fixed_student_student_take_course(request):
+    datas = {
+
+    }
+    return render(request,'pages/fixed-student-student-take-course.html',datas)
+
+def fixed_student_take_course(request):
+    datas = {
+
+    }
+    return render(request,'pages/fixed-student-take-course.html',datas)
+
+def fixed_student_take_quiz(request):
+    datas = {
+
+    }
+    return render(request,'pages/fixed-student-take-quiz.html',datas)
+
+def fixed_student_view_course(request):
+    datas = {
+
+    }
+    return render(request,'pages/fixed-student-view-course.html',datas)
